857101133358e5d98765cc6f692d360bf843a0e7
Andrea Lazarevic <andrea.lazarevic1996@gmail.com>
2021-05-14 18:24:49 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -8,13 +8,7 @@ import pyrebase
 
 def configureDatabase():
     config = {
-        "apiKey": "AIzaSyAZBC4k64qsqU39os0DPtyvJawbzJ-py1Y",
-        "authDomain": "app-mobilno.firebaseapp.com",
-        "databaseURL": "https://app-mobilno.firebaseio.com",
-        "projectId": "app-mobilno",
-        "storageBucket": "app-mobilno.appspot.com",
-        "messagingSenderId": "506686537286",
-        "appId": "1:506686537286:web:710a41323e719455afd447"
+        
     }
 
     firebase = pyrebase.initialize_app(config)
